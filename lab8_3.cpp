9069277487377a63ae2495d1357cf49d182ac1f6
BestChennn <bestchenbestchen@gmail.com>
2021-12-03 15:29:25 +0700

    Complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x){ 
+	if( x == 65){
+		return x = 'Z';
+	}
+	if(x >= 65 and x <=90 ){
+		x = x-1;
+	}else{
+		return '0';
+	}
+
+	return x ;
+
+
+
 }
 
 int main(){
